0e906701a7225a8463ddc6cc0387f06027016916
Mikhail Baylov <mikhailbaylov@gmail.com>
2026-05-12 09:12:52 +0300

    Due to java-faker random data: - change build.gradle; - change TestData; - change ApfTests, TextBoxTests.

--- src/test/java/testData/TestData.java
+++ src/test/java/testData/TestData.java
@@ -1,21 +1,41 @@
 package testData;
 
+import com.github.javafaker.Faker;
+
 public class TestData {
-    public static String
-            firstName = "Mikhail",
-            lastName = "Baylov",
-            userEmail = "mikhailbaylov@qa.gu",
-            userNumber = "7000000000",
-            currentAddress = "bum",
-            gender = "Male",
-            month = "December",
-            year = "1986",
-            day= "16",
-            hobbies = "Music",
-            subjects = "English",
-            state = "Rajasthan",
-            city="Jaipur";
-    public static String fullUserName = firstName + " " + lastName;
-    public static String fullUserDate = day + " " + month + "," + year;
-    public static String stateCity = state + " " + city;
+    Faker faker = new Faker();
+    public String firstName = faker.name().firstName();
+    public String lastName = faker.name().lastName();
+    public String fullUserName = firstName + " " + lastName;
+    public String userEmail = faker.internet().emailAddress();
+    public String userNumber = faker.phoneNumber().subscriberNumber(10);
+    public String currentAddress = faker.address().fullAddress();
+    public String gender = faker.options().option("Male", "Female", "Other");
+    public String year = String.format(String.valueOf(faker.number().numberBetween(1900,2100)));
+    public String month = faker.options().option("January", "February", "March", "April", "May", "June", "July",
+            "August", "September", "October", "November", "December");
+    public String day = String.format(String.valueOf(faker.number().numberBetween(1,28)));
+    public String fullUserDate = day + " " + month + "," + year;
+    public String hobbies = faker.options().option("Sports", "Reading", "Music");
+    public String subjects = faker.options().option("English", "Chemistry", "Computer Science", "Commerce",
+            "Economics", "Social Studies");
+    public String state = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
+    public String city = chooseCityofState();
+    public String stateCity = state + " " + city;
+
+
+    public String chooseCityofState() {
+        String city = selectCity(state);
+        return city;
+    }
+
+    public String selectCity(String state) {
+        return switch (state) {
+            case "NCR" -> faker.options().option("Delhi", "Gurgaon", "Noida");
+            case "Uttar Pradesh" -> faker.options().option("Agra", "Lucknow", "Merrut");
+            case "Haryana" -> faker.options().option("Karnal", "Panipat");
+            case "Rajasthan" -> faker.options().option("Jaipur", "Jaiselmer");
+            default -> null;
+        };
+    }
 }
--- src/test/java/tests/ApfTests.java
+++ src/test/java/tests/ApfTests.java
@@ -1,39 +1,40 @@
 package tests;
 
 import org.junit.jupiter.api.Test;
-import static testData.TestData.*;
+import testData.TestData;
 
 public class ApfTests extends TestBase {
-
+    TestData testData = new TestData();
     @Test
     void apfTestAllFields() {
         apfPage.openPage();
         apfPage.
-                typeFirstName(firstName).
-                typeLastName(lastName).
-                typeUserEmail(userEmail).
-                typeUserGender(gender).
-                typeUserNumber(userNumber).
-                typeDateOfBirth(year,month,day).
-                typeSubjects(subjects).
-                typeUserHobbies(hobbies).uploadPicture("picture.jpg").
-                typeAddress(currentAddress).
-                typeState(state).
-                typeCity(city).
+                typeFirstName(testData.firstName).
+                typeLastName(testData.lastName).
+                typeUserEmail(testData.userEmail).
+                typeUserGender(testData.gender).
+                typeUserNumber(testData.userNumber).
+                typeDateOfBirth(testData.year, testData.month, testData.day).
+                typeSubjects(testData.subjects).
+                typeUserHobbies(testData.hobbies).
+                uploadPicture("picture.jpg").
+                typeAddress(testData.currentAddress).
+                typeState(testData.state).
+                typeCity(testData.city).
 
                 submitForm().
 
                 checkTitleOutput("Thanks for submitting the form").
-                checkFieldsOutput("Student Name",fullUserName).
-                checkFieldsOutput("Student Email",userEmail).
-                checkFieldsOutput("Gender",gender).
-                checkFieldsOutput("Mobile",userNumber).
-                checkFieldsOutput("Date of Birth",fullUserDate).
-                checkFieldsOutput("Subjects",subjects).
-                checkFieldsOutput("Hobbies",hobbies).
+                checkFieldsOutput("Student Name", testData.fullUserName).
+                checkFieldsOutput("Student Email",testData.userEmail).
+                checkFieldsOutput("Gender", testData.gender).
+                checkFieldsOutput("Mobile", testData.userNumber).
+                checkFieldsOutput("Date of Birth",testData.fullUserDate).
+                checkFieldsOutput("Subjects", testData.subjects).
+                checkFieldsOutput("Hobbies", testData.hobbies).
                 checkFieldsOutput("Picture","picture.jpg").
-                checkFieldsOutput("Address",currentAddress).
-                checkFieldsOutput("State and City",stateCity);
+                checkFieldsOutput("Address", testData.currentAddress).
+                checkFieldsOutput("State and City",testData.stateCity);
 
     }
 }
