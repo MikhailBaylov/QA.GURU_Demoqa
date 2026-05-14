2809a4f45d06795cd7364382a086fd708920f70b
Mikhail Baylov <mikhailbaylov@gmail.com>
2026-05-14 10:38:22 +0300

    CalendarPicker changed: new method for day.

--- src/test/java/pages/components/CalendarPicker.java
+++ src/test/java/pages/components/CalendarPicker.java
@@ -9,15 +9,17 @@ public class CalendarPicker {
 
     private final SelenideElement yearOfBirthInput = $("[class=react-datepicker__year-select]");
     private final SelenideElement monthOfBirthInput = $("[class=react-datepicker__month-select]");
-   // private final SelenideElement dayOfBirthInput = $("[class=react-datepicker__month]");
-  //  private final SelenideElement dayRightMonth = $(":not[class=react-datepicker__day--outside-month}");
 
     public CalendarPicker setFullDate(String y, String m, String d) {
         yearOfBirthInput.$(byText(y)).click();
         monthOfBirthInput.$(byText(m)).click();
-        $(".react-datepicker__day--0" + d + ":not(.react-datepicker__day--outside-month").click();
+        dayOfBirthInput(d).click();
         return this;
-        }
-
     }
+
+    private SelenideElement dayOfBirthInput(String d) {
+        return $(".react-datepicker__day--0" + d + ":not(.react-datepicker__day--outside-month)");
+    }
+
+}
 
